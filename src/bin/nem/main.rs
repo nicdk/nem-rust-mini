18b56f812c232a4c21851fc8c5fc16dac6f06437
Kei NIKADO <nikado.kei@gmail.com>
2018-10-01 18:05:59 +0900

    add: add crate log and slap.

--- src/bin/nem/main.rs
+++ src/bin/nem/main.rs
@@ -1,5 +1,20 @@
 extern crate nem;
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+extern crate clap;
+
+use clap::App; 
 
 fn main() {
     println!("Hello.");
+
+    env_logger::init();
+    info!("starting up");
+
+    App::new("myapp")
+        .version("1.0")
+        .about("Does great things!")
+        .author("Kevin K.")
+        .get_matches();     
 }
