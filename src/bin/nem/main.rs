69033e18472a9dedda984667661f75e68fe63ffa
Kei NIKADO <nikado.kei@gmail.com>
2018-10-02 11:23:04 +0900

    update clap parameters same to nem-cli.js

--- src/bin/nem/main.rs
+++ src/bin/nem/main.rs
@@ -4,7 +4,7 @@ extern crate log;
 extern crate env_logger;
 extern crate clap;
 
-use clap::App; 
+use clap::{Arg, App, SubCommand};
 
 fn main() {
     println!("Hello.");
@@ -12,9 +12,25 @@ fn main() {
     env_logger::init();
     info!("starting up");
 
-    App::new("myapp")
-        .version("1.0")
-        .about("Does great things!")
-        .author("Kevin K.")
-        .get_matches();     
+    // https://github.com/evias/nem-cli/blob/master/core/nem-cli.js
+    let matches = App::new("nem-mini")
+        .version("0.1.0")
+        .about("commandline nem client")
+        .subcommand(SubCommand::with_name("list"))
+        .subcommand(SubCommand::with_name("api")
+                    .arg(Arg::with_name("url")
+                    .long("url")))
+        .subcommand(SubCommand::with_name("wallet")
+                    .arg(Arg::with_name("address")
+                    .long("address")))
+        .arg(Arg::with_name("node")
+             .long("node"))
+        .arg(Arg::with_name("port")
+             .long("port"))
+        .arg(Arg::with_name("network")
+             .long("network")
+             .value_name("network"))
+        .get_matches();
+
+    println!("matches {:?}", matches);
 }
